89a9ee08f259b805a6ad731e4829d9c968d34084
Jack Huang <jack.huang@onlyalt.com>
2021-12-01 08:01:42 -0800

    Refactor day1 integer parsing

--- examples/day1.rs
+++ examples/day1.rs
@@ -1,40 +1,30 @@
 use aoc_2021::utils;
 
-fn part_one(lines: &Vec<String>) -> i32 {
+fn part_one(lines: &Vec<u64>) -> u64 {
     lines
         .iter()
-        .fold((0, None), |(count, prev), x| {
-            let cur_val = x
-                .parse::<i32>()
-                .expect("could not parse invalid integer string");
-
-            match prev {
-                Some(prev_val) if cur_val > prev_val => (count + 1, Some(cur_val)),
-                _ => (count, Some(cur_val)),
-            }
+        .fold((0, None), |(count, prev), x| match prev {
+            Some(prev_val) if x > prev_val => (count + 1, Some(x)),
+            _ => (count, Some(x)),
         })
         .0
 }
 
-fn part_two(lines: &Vec<String>) -> i32 {
+fn part_two(lines: &Vec<u64>) -> u64 {
     lines
         .iter()
         .fold((0, None, None, None), |(count, a, b, c), x| {
-            let cur_val = x
-                .parse::<i32>()
-                .expect("could not parse invalid integer string");
-
             match (a, b, c) {
-                (Some(aa), Some(bb), Some(cc)) => { 
-                    if bb + cc + cur_val > aa + bb + cc {
-                        (count + 1, Some(bb), Some(cc), Some(cur_val))
+                (Some(aa), Some(bb), Some(cc)) => {
+                    if bb + cc + x > aa + bb + cc {
+                        (count + 1, Some(bb), Some(cc), Some(x))
                     } else {
-                        (count, Some(bb), Some(cc), Some(cur_val))
+                        (count, Some(bb), Some(cc), Some(x))
                     }
                 }
-                (None, Some(bb), Some(cc)) => (count, Some(bb), Some(cc), Some(cur_val)),
-                (None, None, Some(cc)) => (count, None, Some(cc), Some(cur_val)),
-                _ => (count, None, None, Some(cur_val)),
+                (None, Some(bb), Some(cc)) => (count, Some(bb), Some(cc), Some(x)),
+                (None, None, Some(cc)) => (count, None, Some(cc), Some(x)),
+                _ => (count, None, None, Some(x)),
             }
         })
         .0
@@ -42,7 +32,14 @@ fn part_two(lines: &Vec<String>) -> i32 {
 
 fn main() {
     let lines = utils::load_lines("inputs/day1").expect("could not load input");
+    let depths = lines
+        .iter()
+        .map(|x| {
+            x.parse::<u64>()
+                .expect("could not parse invalid integer string")
+        })
+        .collect::<Vec<u64>>();
 
-    println!("part one: {}", part_one(&lines));
-    println!("part two: {}", part_two(&lines));
+    println!("part one: {}", part_one(&depths));
+    println!("part two: {}", part_two(&depths));
 }
