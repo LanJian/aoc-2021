54d989f5561404bb70b614813c0a2160ddf5cda2
Jack Huang <jack.huang@onlyalt.com>
2021-12-23 21:38:51 -0800

    Update name

--- benches/day_08.rs
+++ benches/day_08.rs
@@ -4,7 +4,7 @@ use aoc_2021::{
 use criterion::{black_box, criterion_group, Criterion};
 
 fn benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("day 08: REPLACE ME");
+    let mut group = c.benchmark_group("day 08: seven segment search");
 
     group.bench_function("part 1", |b| {
         let lines = utils::load_input("inputs/day_08").expect("could not load input");
